6c14b68e09f7a59351881790e04875944aab5930
HoYangE <syhwms@naver.com>
2023-08-12 18:11:48 +0900

    2일차

---
+++ Assets/Scripts/TextList.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using TMPro;
+
+public class TextList : MonoBehaviour
+{   
+    [SerializeField]
+    private GameObject listParent;
+    [SerializeField]
+    private GameObject textPrefab;
+
+    [SerializeField]
+    private Vector3 startPosition;
+    [SerializeField]
+    private int maxTextCount;
+
+    public void NewText(string text)
+    {
+        var textObject = Instantiate(textPrefab, listParent.transform);
+        textObject.transform.SetAsFirstSibling();
+        textObject.GetComponent<TMP_Text>().text = text;
+        PushText();
+    }
+
+    private void PushText()
+    {
+        for (var i = 0; i < listParent.transform.childCount; i++)
+        {
+            if (i >= maxTextCount)
+            {
+                Destroy(listParent.transform.GetChild(i).gameObject);
+                break;
+            }
+
+            var textObject = listParent.transform.GetChild(i);
+            textObject.transform.localPosition = new Vector3(0, startPosition.y - 70 * i, 0);
+        }
+    }
+}
